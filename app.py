7d065b5951a4f9e8acbcbdb48d74e80c3e3408cc
Utkarsh Mishra <iamutkarshmisrha@gmail.com>
2018-08-01 16:04:43 +0530

    flask app

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ from flask import Flask, render_template, redirect, \
 from functools import wraps
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from forms import LoginForm
+from flask_login import LoginManager, login_user, login_required , logout_user, current_user
+from forms import LoginForm, RegisterForm, MessageForm
+import os
 
 # create the application object, pass it into Bcrypt for hashing
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 # config
-import os
 app.config.from_object('config.DevelopmentConfig')
 
 # create the sqlalchemy object
@@ -20,26 +23,43 @@ db = SQLAlchemy(app)
 # import db schema
 from models import *
 
+login_manager.login_view = "login"
 
-# login required decorator
-def login_required(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return f(*args, **kwargs)
-        else:
-            flash('You need to login first.')
-            return redirect(url_for('login'))
-    return wrap
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter(User.id == int(user_id)).first()
+
+# # login required decorator
+# def login_required(f):
+#     @wraps(f)
+#     def wrap(*args, **kwargs):
+#         if 'logged_in' in session:
+#             return f(*args, **kwargs)
+#         else:
+#             flash('You need to login first.')
+#             return redirect(url_for('login'))
+#     return wrap
 
 
 # use decorators to link the function to a url
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    # return "Hello, World!"  # return a string
-    posts = db.session.query(BlogPost).all()
-    return render_template('index.html', posts=posts)  # render a template
+    error = None
+    form = MessageForm(request.form)
+    if form.validate_on_submit():
+        new_message = BlogPost(
+            form.title.data,
+            form.description.data,
+            current_user.id
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        flash('New entry was successfully posted. Thanks.')
+        return redirect(url_for('home'))
+    else:
+        posts = db.session.query(BlogPost).all()
+        return render_template('index.html', posts=posts, form = form, error = error)  # render a template
 
 
 @app.route('/welcome')
@@ -57,21 +77,37 @@ def login():
             user = User.query.filter_by(name = request.form['username']).first()
             if user is not None and bcrypt.check_password_hash(user.password,request.form['password']):
             # if (request.form['username'] != 'admin') or request.form['password'] != 'admin':
-                session['logged_in'] = True
+                # session['logged_in'] = True
+                login_user(user)
                 flash('You were logged in.')
                 return redirect(url_for('home'))
-        else:
-            error = 'Invalid Credentials. Please try again.'
+            else:
+                error = 'Invalid username or password.'
     return render_template('login.html',form = form, error=error)
 
 
 @app.route('/logout')
 @login_required
 def logout():
-    session.pop('logged_in', None)
+    # session.pop('logged_in', None)
+    logout_user()
     flash('You were logged out.')
     return redirect(url_for('welcome'))
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(
+            name=form.username.data,
+            email=form.email.data,
+            password=form.password.data
+        )
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        return redirect(url_for('home'))
+    return render_template('register.html', form=form)
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from app import db, bcrypt
+from app import db
+from app import bcrypt
 
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
@@ -33,6 +34,18 @@ class User(db.Model):
         self.name = name
         self.email = email
         self.password = bcrypt.generate_password_hash(password)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
 
     def __repr__(self):
         return '<name {}'.format(self.name)
